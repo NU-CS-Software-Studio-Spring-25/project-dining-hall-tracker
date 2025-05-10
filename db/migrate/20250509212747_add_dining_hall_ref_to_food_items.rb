2ea07703505e618a5cb6d71e859ebaf89b676b8d
taeoldest <alextaeyounglee@gmail.com>
2025-05-10 15:39:57 -0500

    fixed migration

--- db/migrate/20250509212747_add_dining_hall_ref_to_food_items.rb
+++ db/migrate/20250509212747_add_dining_hall_ref_to_food_items.rb
@@ -1,5 +1,6 @@
 class AddDiningHallRefToFoodItems < ActiveRecord::Migration[8.0]
   def change
+    FoodItem.delete_all
     add_reference :food_items, :dining_hall, null: false, foreign_key: true
   end
 end
