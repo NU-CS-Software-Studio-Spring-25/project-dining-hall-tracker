2086e7aba65c8a0bf9a881e195604e38857cd747
Jason Latz <159408658+Jason-Latz@users.noreply.github.com>
2025-05-12 14:53:01 -0500

    attempting build integration

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,23 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-// import MinimalApp from './MinimalApp'
+import App from './App'
 
 console.log('Main script is running')
 console.log('Root element:', document.getElementById('root'))
 
 const rootElement = document.getElementById('root')
-if (rootElement) {
-  console.log('Creating root')
-  const root = createRoot(rootElement)
-  console.log('About to render App component')
-  root.render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  )
-  console.log('Render called')
-} else {
-  console.error('Root element not found!')
+if (!rootElement) {
+  throw new Error('Root element not found!')
 }
+
+console.log('Creating root')
+const root = createRoot(rootElement)
+console.log('About to render App component')
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
+console.log('Render called')
