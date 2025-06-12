70da3fc8e3bf191b8af915414f9947e5a28f63ae
Prashant Ghimire <111095724+Prsnt95@users.noreply.github.com>
2025-06-11 20:45:44 -0500

    Fix password reset redirect in backend controller (correct location)

--- backend/app/controllers/users/passwords_controller.rb
+++ backend/app/controllers/users/passwords_controller.rb
@@ -22,15 +22,39 @@ class Users::PasswordsController < Devise::PasswordsController
 
   # GET /users/password/edit?reset_password_token=abcdef
   def edit
-    self.resource = resource_class.new
-    set_minimum_password_length
-    resource.reset_password_token = params[:reset_password_token]
+    Rails.logger.info "PASSWORD RESET: Redirecting token #{params[:reset_password_token]} to frontend"
     
-    # For API, just return the token validation
-    render json: {
-      message: 'Reset token is valid',
-      reset_password_token: params[:reset_password_token]
-    }, status: :ok
+    # Prevent caching
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    
+    # Use HTML + JavaScript redirect as absolute fallback
+    render html: %{
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>Redirecting...</title>
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+        <meta http-equiv="Pragma" content="no-cache">
+        <meta http-equiv="Expires" content="0">
+        <script>
+          console.log('Redirecting to password reset page...');
+          window.location.href = '/reset-password?reset_password_token=#{params[:reset_password_token]}';
+        </script>
+      </head>
+      <body>
+        <h2>Redirecting to password reset...</h2>
+        <p>If not redirected automatically, <a href="/reset-password?reset_password_token=#{params[:reset_password_token]}">click here</a></p>
+        <script>
+          // Backup redirect in case the first one doesn't work
+          setTimeout(function() {
+            window.location.replace('/reset-password?reset_password_token=#{params[:reset_password_token]}');
+          }, 1000);
+        </script>
+      </body>
+      </html>
+    }.html_safe
   end
 
   # PUT /users/password
