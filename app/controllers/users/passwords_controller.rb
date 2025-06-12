c1a1f11a9a85b0ba30099ae7e874c2e2b9d36181
Prashant Ghimire <111095724+Prsnt95@users.noreply.github.com>
2025-06-11 20:40:50 -0500

    Add cache-busting headers and enhanced redirect logic

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -5,19 +5,34 @@ class Users::PasswordsController < Devise::PasswordsController
   def edit
     Rails.logger.info "PASSWORD RESET: Redirecting token #{params[:reset_password_token]} to frontend"
     
+    # Prevent caching
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    
     # Use HTML + JavaScript redirect as absolute fallback
     render html: %{
       <!DOCTYPE html>
       <html>
       <head>
         <title>Redirecting...</title>
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+        <meta http-equiv="Pragma" content="no-cache">
+        <meta http-equiv="Expires" content="0">
         <script>
+          console.log('Redirecting to password reset page...');
           window.location.href = '/reset-password?reset_password_token=#{params[:reset_password_token]}';
         </script>
       </head>
       <body>
-        <p>Redirecting to password reset...</p>
+        <h2>Redirecting to password reset...</h2>
         <p>If not redirected automatically, <a href="/reset-password?reset_password_token=#{params[:reset_password_token]}">click here</a></p>
+        <script>
+          // Backup redirect in case the first one doesn't work
+          setTimeout(function() {
+            window.location.replace('/reset-password?reset_password_token=#{params[:reset_password_token]}');
+          }, 1000);
+        </script>
       </body>
       </html>
     }.html_safe
