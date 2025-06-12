5877549aea41fee723f149b4c98055ed89bb5e71
Prashant Ghimire <111095724+Prsnt95@users.noreply.github.com>
2025-06-11 20:37:08 -0500

    Use HTML + JavaScript redirect as guaranteed fallback

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -5,8 +5,21 @@ class Users::PasswordsController < Devise::PasswordsController
   def edit
     Rails.logger.info "PASSWORD RESET: Redirecting token #{params[:reset_password_token]} to frontend"
     
-    # Force redirect with 302 status
-    redirect_to "/reset-password?reset_password_token=#{params[:reset_password_token]}", status: 302
-    return
+    # Use HTML + JavaScript redirect as absolute fallback
+    render html: %{
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>Redirecting...</title>
+        <script>
+          window.location.href = '/reset-password?reset_password_token=#{params[:reset_password_token]}';
+        </script>
+      </head>
+      <body>
+        <p>Redirecting to password reset...</p>
+        <p>If not redirected automatically, <a href="/reset-password?reset_password_token=#{params[:reset_password_token]}">click here</a></p>
+      </body>
+      </html>
+    }.html_safe
   end
 end 
