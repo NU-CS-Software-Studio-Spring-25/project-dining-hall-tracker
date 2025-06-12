2a2c55fd65d70e075a4f43c253bcf19676ad9246
Prashant Ghimire <111095724+Prsnt95@users.noreply.github.com>
2025-06-11 19:53:31 -0500

    Add debugging logs to password reset edit action

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -31,8 +31,13 @@ class Users::PasswordsController < Devise::PasswordsController
 
   # GET /users/password/edit?reset_password_token=abcdef
   def edit
+    # Log debugging information
+    Rails.logger.info "PASSWORD RESET DEBUG: params[:format] = #{params[:format].inspect}"
+    Rails.logger.info "PASSWORD RESET DEBUG: request.xhr? = #{request.xhr?.inspect}"
+    
     # For password reset links from email, always redirect to frontend unless explicitly JSON
     if params[:format] == 'json'
+      Rails.logger.info "PASSWORD RESET DEBUG: Returning JSON response"
       # Only return JSON for explicit JSON format requests
       self.resource = resource_class.new
       set_minimum_password_length
@@ -43,6 +48,7 @@ class Users::PasswordsController < Devise::PasswordsController
         reset_password_token: params[:reset_password_token]
       }, status: :ok
     else
+      Rails.logger.info "PASSWORD RESET DEBUG: Redirecting to frontend"
       # For all other requests (including email links and wildcard formats), redirect to frontend
       redirect_to "/reset-password?reset_password_token=#{params[:reset_password_token]}"
     end
